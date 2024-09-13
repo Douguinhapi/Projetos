925bdb15650f084b77acf913d827d5b1dff371bf
Douguinhapi <douguinhapi123@gmail.com>
2024-09-13 16:00:59 -0300

    thyt

--- EnsinoMedio/App/app/Pokemon/index.jsx
+++ EnsinoMedio/App/app/Pokemon/index.jsx
@@ -1,22 +1,121 @@
-import React from 'react';
-import { Button, View, StyleSheet } from 'react-native';
-import { Link } from 'expo-router';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, ImageBackground } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
 
-export default function App() {
+export default Seletor = () => {
+  const [pokemon, setPokemon] = useState('');
+  const [listaPokemons, setListaPokemons] = useState([]);
+  const [pokemonsIniciais, setPokemonsIniciais] = useState([]);
+  const [tipos, setTipos] = useState([]);
+  const [tipoSelecionado, setTipoSelecionado] = useState('');
+
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/type')
+      .then((response) => response.json())
+      .then((dados) => setTipos(dados.results))
+      .catch((error) => console.error('Erro ao buscar os tipos:', error));
+
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+      .then((response) => response.json())
+      .then((dados) => {
+        setListaPokemons(dados.results);
+        setPokemonsIniciais(dados.results);
+      })
+      .catch((error) => console.error('Erro ao buscar os pokémons:', error));
+  }, []);
+
+  useEffect(() => {
+    if (tipoSelecionado) {
+      fetch(`https://pokeapi.co/api/v2/type/${tipoSelecionado}`)
+        .then((response) => response.json())
+        .then((dados) => {
+          const pokemonsPorTipo = dados.pokemon.map((p) => p.pokemon.name);
+
+          const pokemonsFiltrados = pokemonsIniciais.filter((pokemon) =>
+            pokemonsPorTipo.includes(pokemon.name)
+          );
+
+          setListaPokemons(pokemonsFiltrados);
+        })
+        .catch((error) =>
+          console.error('Erro ao buscar os pokémons por tipo:', error)
+        );
+    } else {
+      setListaPokemons(pokemonsIniciais);
+    }
+  }, [tipoSelecionado, pokemonsIniciais]);
+
+  const image = { uri: "https://wallpaper-house.com/data/out/7/wallpaper2you_147633.jpg" };
+
   return (
-    <View style={styles.container}>
-      <View style={styles.buttonContainer}>
-        <Link href="./Banco">
-          <Button title="Santander" onPress={() => { }} />
-        </Link>
-        <Link href="./Calculadora">
-          <Button title="Calculadora" onPress={() => { }} />
-        </Link>
-        <Link href="./SingUp">
-          <Button title="SingUp" onPress={() => { }} />
-        </Link>
+    <ImageBackground source={image} style={styles.backgroundImage}>
+      <View style={styles.container}>
+      <Text style={styles.title}>Selecione um Pokémon</Text>
+        <Picker
+          selectedValue={tipoSelecionado}
+          onValueChange={(itemValue) => setTipoSelecionado(itemValue)}
+          style={styles.picker}
+          itemStyle={styles.pickerItem}
+        >
+          <Picker.Item label='Selecione um Tipo' value='' />
+          {tipos.map((tipo, index) => (
+            <Picker.Item key={index} label={tipo.name} value={tipo.name} />
+          ))}
+        </Picker>
+
+        <Picker
+          selectedValue={pokemon}
+          onValueChange={(itemValue) => setPokemon(itemValue)}
+          style={styles.picker}
+          itemStyle={styles.pickerItem}
+        >
+          <Picker.Item label='Selecione um Pokémon' value='' />
+          {listaPokemons.map((item, index) => (
+            <Picker.Item key={index} label={item.name} value={item.name} />
+          ))}
+        </Picker>
+
+        {pokemon ? <Text style={styles.selectedText}>Você selecionou {pokemon}</Text> : ''}
       </View>
-      {/* tete */}
-    </View>
+    </ImageBackground>
   );
 };
+
+const styles = StyleSheet.create({
+  backgroundImage: {
+    flex: 1,
+    resizeMode: 'cover',
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  title: {
+    fontSize: 26,
+    fontWeight: '600',
+    color: '#ffffff',
+    marginBottom: 10,
+  },
+  picker: {
+    width: '50%',
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    borderColor: '#ddd',
+    borderWidth: 1,
+    elevation: 2,
+    marginBottom: 15,
+  },
+  pickerItem: {
+    height: 50,
+    fontSize: 18,
+    color: '#333',
+  },
+  selectedText: {
+    fontSize: 20,
+    color: '#ffffff',
+    marginTop: 20,
+    fontWeight: 'bold',
+  },
+});
