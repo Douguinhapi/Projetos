a008ffe6266aff564415be42665106d7641c86a2
Douguinhapi <douguinhapi123@gmail.com>
2024-09-13 15:04:52 -0300

    pokemon

---
+++ EnsinoMedio/App/app/Pokemon/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Button, View, StyleSheet } from 'react-native';
+import { Link } from 'expo-router';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.buttonContainer}>
+        <Link href="./Banco">
+          <Button title="Santander" onPress={() => { }} />
+        </Link>
+        <Link href="./Calculadora">
+          <Button title="Calculadora" onPress={() => { }} />
+        </Link>
+        <Link href="./SingUp">
+          <Button title="SingUp" onPress={() => { }} />
+        </Link>
+      </View>
+    </View>
+  );
+};
