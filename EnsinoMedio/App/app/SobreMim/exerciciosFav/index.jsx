ba78302e06b1101811810bc546054e6edf8c4e30
Douguinhapi <douguinhapi123@gmail.com>
2024-09-18 14:51:46 -0300

    ss

--- EnsinoMedio/App/app/SobreMim/exerciciosFav/index.jsx
+++ EnsinoMedio/App/app/SobreMim/exerciciosFav/index.jsx
@@ -1,11 +1,21 @@
-import { View, Text, StyleSheet, ImageBackground, Image } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 
 export default function () {
 
   return (
-      <View style={styles.container}>
-        <Text>Qualquer coisa</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>- Meus Exercícios Favoritos -</Text>
+      <View style={styles.gifs}>
+        <Image style={styles.profileImage} source={require('./1.gif')} />
+        <Text>Remada Unilateral</Text>
+        <Image style={styles.profileImage} source={require('./2.webp')} />
+        <Text>Agachamento Livre</Text>
+        <Image style={styles.profileImage} source={require('./3.webp')} />
+        <Text>Rosca Bíceps</Text>
+        <Image style={styles.profileImage} source={require('./4.webp')} />
+        <Text>Supino Inclinado</Text>
       </View>
+    </View>
   );
 };
 
@@ -16,4 +26,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: 'white', // Cor de fundo clara e suave
   },
+  title: {
+    color: 'purple',
+    fontWeight: 'bold',
+    fontSize: 48,
+  },
+  profileImage: {
+    width: 100,
+  }
 });
